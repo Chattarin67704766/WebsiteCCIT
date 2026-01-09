8bd705253b28a7d252ab9cc301bbcc5974d21394
Chattarin67704766 <Chainnx04@gmail.com>
2026-01-09 17:45:02 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -141,4 +141,15 @@ document.addEventListener('DOMContentLoaded', () => {
         // Initialize
         startTimer();
     }
+
+    // --- Live Chat Widget (Tawk.to) ---
+    var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+    (function () {
+        var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+        s1.async = true;
+        s1.src = 'https://embed.tawk.to/6960db761d2981197cba4eee/default'; // User Provided Property ID
+        s1.charset = 'UTF-8';
+        s1.setAttribute('crossorigin', '*');
+        s0.parentNode.insertBefore(s1, s0);
+    })();
 });
